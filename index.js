add8dbeb1e9a894c03ca611538026ace65870b40
abhijadon <134587271+abhijadon@users.noreply.github.com>
2023-08-09 03:42:40 +0530

    (09-08-2023)

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
-
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
 const port = 5000;
 app.use(bodyParser.json());
+app.use(cors({ origin: "http://localhost:3000" }));
 
 //Api routes are available
 app.use("/api", require("./routes/sheet"));
+app.use("/api", require("./routes/form"));
 
 //connect to localhost
 
---
+++ middleware/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose"); // new
+
+// Connect to MongoDB database
+mongoose
+  .connect("mongodb://localhost:27017/acmedb", { useNewUrlParser: true })
+  .then(() => {
+    const app = express();
+
+    app.listen(5000, () => {
+      console.log("Server has started!");
+    });
+  });
