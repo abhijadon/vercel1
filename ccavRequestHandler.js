8d6e037df55f25ea4d6bdbe17f54120cba882245
abhijadon <134587271+abhijadon@users.noreply.github.com>
2023-09-15 10:53:20 +0530

    this

--- ccavRequestHandler.js
+++ ccavRequestHandler.js
@@ -1,25 +1,42 @@
-var http = require('http'),
-    fs = require('fs'),
-    ccav = require('./ccavutil.js'),
-    qs = require('querystring');
+var http = require("http"),
+  fs = require("fs"),
+  ccav = require("./ccavutil.js"),
+  qs = require("querystring");
 
-exports.postReq = function(request,response){
-    var body = "",
-      workingKey = "9223AAA9021800C10C706B47E6B0D7C3", //Put in the 32-Bit key shared by CCAvenues.
-      accessCode = "AVXS76JC64CK33SXKC", //Put in the Access Code shared by CCAvenues.
-      encRequest = "",
-      formbody = "";
-				
-    request.on('data', function (data) {
-	body += data;
-	encRequest = ccav.encrypt(body,workingKey); 
-	formbody = '<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' + encRequest + '"><input type="hidden" name="access_code" id="access_code" value="' + accessCode + '"><script language="javascript">document.redirect.submit();</script></form>';
+exports.postReq = function (request, response) {
+  const { name, amount } = request.body;
+  var workingKey = "9223AAA9021800C10C706B47E6B0D7C3", //Put in the 32-Bit key shared by CCAvenues.
+    accessCode = "AVXS76JC64CK33SXKC"; //Put in the Access Code shared by CCAvenues.
+
+  if (request.method === "POST") {
+    var body = [
+      {
+        name,
+        amount,
+      },
+    ];
+
+    request.on("data", function (data) {
+      body += data;
     });
-				
-    request.on('end', function () {
-        response.writeHeader(200, {"Content-Type": "text/html"});
-	response.write(formbody);
-	response.end();
+
+    request.on("end", function () {
+      if (body) {
+        var encRequest = ccav.encrypt(body, workingKey);
+        var formbody =
+          '<form id="nonseamless" method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"/> <input type="hidden" id="encRequest" name="encRequest" value="' +
+          encRequest +
+          '"><input type="hidden" name="access_code" id="access_code" value="' +
+          accessCode +
+          '"><input type="submit" value="Submit"></form>';
+
+        response.setHeader("Content-Type", "text/html");
+        response.end(formbody);
+      } else {
+        response.status(400).end("Bad Request");
+      }
     });
-   return; 
+  } else {
+    response.status(405).end("Method Not Allowed");
+  }
 };
